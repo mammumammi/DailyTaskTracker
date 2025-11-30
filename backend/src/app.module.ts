ac692fbbb28cd7373d4ba8f6141406f816378f54
mammumammi <ashinmammu@gmail.com>
2025-11-30 11:32:42 +0530

    fixed backend issues

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoginModule } from './login/login.module';
-import { TypeOrmModule} from '@nestjs/typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './login/user.entity';
 import { TaskModule } from './task/task.module';
 import { CategoryModule } from './category/category.module';
@@ -10,13 +10,28 @@ import { Task } from './entities/task.entity';
 import { Category } from './entities/category.entity';
 import { AuthModule } from './auth/auth.module';
 import { typeOrmConfig } from './ormconfig';
-import {ConfigModule} from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRoot(typeOrmConfig),LoginModule, TaskModule, CategoryModule,AuthModule
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env.local',
+    })
+    ,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async () => ({
+        type: 'postgres',
+        url: process.env.DATABASE_URL,
+        autoLoadEntities: true,
+        synchronize: true,
+        ssl: false,
+        logging: true,
+      }),
+    }),
+    LoginModule, TaskModule, CategoryModule, AuthModule
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
